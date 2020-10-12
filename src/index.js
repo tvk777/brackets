116cf21c13b6f5643210b069d7f99f252a96068d
tvk777 <klimenko.tv7@gmail.com>
2020-10-12 21:06:41 +0300

    feat: begin task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const strToArray = str.split('');
+  bracketsConfig.forEach(item => {
+    const openCount = strToArray.filter(s => s === item[0]).length;
+    const closeCount = strToArray.filter(s => s === item[1]).length;
+    if (openCount !== closeCount) return false;
+  })
+  return true;
 }
