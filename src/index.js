9e1ce76e444b11273c8f44bd7269a04d1314ec7e
tvk777 <klimenko.tv7@gmail.com>
2020-10-13 21:23:53 +0300

    feat: complete function check

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
   const strToArray = str.split('');
-  bracketsConfig.forEach(item => {
-    const openCount = strToArray.filter(s => s === item[0]).length;
-    const closeCount = strToArray.filter(s => s === item[1]).length;
-    if (openCount !== closeCount) return false;
-  })
+  if(strToArray.length % 2 !==0) return false;
+
+  let openBrackets = [];
+  let closeBrackets = [];
+  let currentBrackets = [];
+
+  bracketsConfig.forEach(array => {
+    openBrackets.push(array[0]);
+    closeBrackets.push(array[1]);
+  });
+
+  for (let i = 0; i < strToArray.length; i++) {
+    if (openBrackets.includes(strToArray[i]) && closeBrackets.includes(strToArray[i])) {
+      if (!currentBrackets.includes(strToArray[i])) {
+        currentBrackets.push(strToArray[i]);
+      } else if (currentBrackets.pop() === strToArray[i]) {
+        continue;
+      } else {
+        return false;
+      }
+    } else {
+      if (openBrackets.includes(strToArray[i])) {
+        currentBrackets.push(strToArray[i]);
+      } else if (openBrackets.indexOf(currentBrackets.pop()) === closeBrackets.indexOf(strToArray[i])) { continue; }
+      else {
+        return false;
+      }
+    }
+  }
+
   return true;
 }
